8c074a8457b12cfc79a903dc66c2295e38927332
David Bruant <bruant.d@gmail.com>
2013-12-27 17:20:45 +0100

    graph in front-end

--- extension/js/main.js
+++ extension/js/main.js
@@ -1,7 +1,37 @@
+"use strict";
 document.addEventListener('DOMContentLoaded', function (e) {
-    "use strict";
-    console.log('DOMContentLoaded');
-    document.body.textContent = 'loaded bitches!';
-    throw new Error('TODO');
+    console.log('graphViz loaded?', typeof graphViz.updateGraph === "function");
+
+    var nodes = [{}, {}, {}, {}, {}, {}, {}];
+    graphViz.updateGraph(nodes, [
+        {
+            source: nodes[0],
+            target: nodes[1]
+        },
+        {
+            source: nodes[0],
+            target: nodes[2]
+        },
+        {
+            source: nodes[2],
+            target: nodes[3]
+        },
+        {
+            source: nodes[1],
+            target: nodes[4]
+        },
+        {
+            source: nodes[4],
+            target: nodes[5]
+        },
+        {
+            source: nodes[5],
+            target: nodes[2]
+        }
+    ]);
+
+    window.addEventListener('message', function (e) {
+        throw 'TODO';
+    });
 });
 //# sourceMappingURL=main.js.map
--- extension/js/main.ts
+++ extension/js/main.ts
@@ -1,6 +1,44 @@
+"use strict";
+
+declare var graphViz;
+
 document.addEventListener('DOMContentLoaded', e => {
-  "use strict";
-  console.log('DOMContentLoaded')
-  document.body.textContent = 'loaded bitches!';
-  throw new Error('TODO');
-});
+  
+  console.log('graphViz loaded?', typeof graphViz.updateGraph === "function");
+  
+  var nodes = [{},{},{},{},{},{},{}];
+  graphViz.updateGraph(nodes, [
+    {
+      source: nodes[0],
+      target: nodes[1]
+    },
+    {
+      source: nodes[0],
+      target: nodes[2]
+    },
+    {
+      source: nodes[2],
+      target: nodes[3]
+    },
+    {
+      source: nodes[1],
+      target: nodes[4]
+    },
+    {
+      source: nodes[4],
+      target: nodes[5]
+    },
+    {
+      source: nodes[5],
+      target: nodes[2]
+    }
+  ]);
+  
+  window.addEventListener('message', e => {
+    //graphViz.updateGraph(e.nodes, e.edges);
+    throw 'TODO';
+  });
+  
+})
+
+
