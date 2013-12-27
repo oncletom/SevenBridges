653dd3de782704ca66b5a956294dd8c7851424bb
oncletom <thomas@oncle-tom.net>
2013-12-27 17:08:53 +0100

    First attempt for a crawler

---
+++ extension/src/bg/crawler.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+class Crawler{
+    private _queue = [];
+
+    constructor(public options:CrawlerOptions){
+    }
+
+    getLinks(url, callback){
+        var links = [];
+
+        this.queue(url);
+
+        callback(links);
+    }
+
+    queue(url){
+        this._queue.push(url);
+    }
+}
+
+export = Crawler;
